d413ccc458f9d45e0e7ad279c67953c364595207
Nathan <nathan.roussel.1@ens.etsmtl.ca>
2021-04-04 21:57:16 -0400

    Working mti api, output configuration and read mtdata2. Reading at 20Hz.

--- app/mti.c
+++ app/mti.c
@@ -10,6 +10,7 @@ static mti_device_state mtiState = 0;
 static volatile mtData2 mtiData;
 //static char g_textBuffer[256];
 static volatile uint8_t test = 0;
+static volatile uint8_t dataNumber = 0;
 
 static void mti_error(){
 
@@ -120,31 +121,99 @@ void config_mti(void){
 #endif
 
 	//Setup OutputConfiguration
+	if(!mti_mtData2_configure()) return;
+
 
 
 }
 
-void read_packet(uint8_t size, , uint8_t* ptr, uint8_t* prm){
+/***********************
+ * MTI API
+ ***********************/
+
+/* FUNCTION TO CREATE */
+
+static void read_packet(uint8_t size, uint8_t* ptr, uint8_t* prm){
 
 	if(size != sizeof(mtiData.xdiPacketCounter)) return; //Check datasize
 	mtiData.xdiPacketCounter = readUint16(ptr, prm);
 
+	sd_writeUint("packet",mtiData.xdiPacketCounter);
+
 }
 
-void read_acceleration(uint8_t size, , uint8_t* ptr, uint8_t* prm){
+static void read_acceleration(uint8_t size, uint8_t* ptr, uint8_t* prm){
 
 	if(size != sizeof(mtiData.xdiAcceleration)) return;
 	mtiData.xdiAcceleration.x = readFloat(ptr, prm);
 	mtiData.xdiAcceleration.y = readFloat(ptr, prm);
 	mtiData.xdiAcceleration.z = readFloat(ptr, prm);
 
+	sd_writeFloat("x",mtiData.xdiAcceleration.x);
+	sd_writeFloat("y",mtiData.xdiAcceleration.y);
+	sd_writeFloat("z",mtiData.xdiAcceleration.z);
+
 }
 
-void read_temperature(uint8_t size, uint8_t* ptr, uint8_t* prm){
+static void read_temperature(uint8_t size, uint8_t* ptr, uint8_t* prm){
 
 	if(size != sizeof(mtiData.xdiTemperature)) return;
 	mtiData.xdiTemperature = readFloat(ptr, prm);
 
+	sd_writeFloat("temp",mtiData.xdiTemperature);
+
+}
+
+/* DATA STRUCTURE TO FILL */
+
+mti_api mti_data[] = {
+
+		XDI_PacketCounter, 0xffff , "NPacket", read_packet,
+		XDI_Acceleration, 20 , "Accel", read_acceleration,
+		XDI_Temperature, 20 , "Temperature", read_temperature
+};
+
+/***********************
+ * END OF MTI API
+ ***********************/
+
+uint8_t mti_mtData2_configure(void){
+
+	uint8_t i = 0;
+	uint8_t nb = 0;
+
+	//See how many data we have
+	nb = sizeof(mti_data)/28;
+
+	//message(xbusMessage, 0xff, XMID_SetOutputConfiguration, 4*nb); //set payload len
+
+	uint8_t* payload = getPointerToPayload(xbusMessage);
+
+	//Add first row
+	for(i = 0; i< nb; i++){
+
+		payload[4*i] = mti_data[i].id >> 8;
+		payload[(4*i)+1] = mti_data[i].id;
+		payload[(4*i)+2] = mti_data[i].frequency >> 8;
+		payload[(4*i)+3] = mti_data[i].frequency;
+
+	}
+
+	XbusMessage msg;
+
+	msg.m_mid = XMID_SetOutputConfiguration;
+	msg.m_length = 4*nb;
+	msg.m_data = payload;
+
+	MtsspInterface_sendXbusMessage(&msg);
+	while(HAL_GPIO_ReadPin(DATA_READY_PORT, DATA_READY_PIN) != GPIO_PIN_SET);
+	mti_receive(XMID_SetOutputConfigurationAck);
+
+#if (configTRANSCRIPT_ENABLED)
+			transcript(APP_MTI_NAME,"Output configuration sent",0);
+#endif
+
+	return 1;
 }
 
 uint8_t mti_mtData2_parse(XbusMessage msg){
@@ -152,6 +221,7 @@ uint8_t mti_mtData2_parse(XbusMessage msg){
 	uint16_t dataId;
 	uint8_t dataSize;
 	uint8_t dataIndex = 0;
+	uint8_t i = 0;
 
 	//Check if it is from MTData 2
 	if(msg.m_mid != XMID_MtData2){
@@ -161,40 +231,25 @@ uint8_t mti_mtData2_parse(XbusMessage msg){
 		return 0;
 	}
 
-	//Loop
+	//Loop to check all data
 	while(dataIndex < msg.m_length){
 
 		//Read message id from pointer
 		dataId = readUint16(msg.m_data, &dataIndex);
 		dataSize = readUint8(msg.m_data, &dataIndex);
 
-		switch(dataId){
+		//Check all data
+		if(mti_data[i].id == dataId){
 
-			case XDI_PacketCounter:
+			mti_data[i].callback(dataSize, msg.m_data, &dataIndex);
 
+		}else{
 
-				break;
+			dataIndex += dataSize; //Skip the data
 
-			case XDI_Acceleration:
-
-
-				break;
-
-			case XDI_Temperature:
-
-				if(dataSize != sizeof(mtiData.xdiTemperature)) break;
-
-				mtiData.xdiTemperature = readFloat(msg.m_data, &dataIndex);
-
-				sd_writeFloat("Temp",mtiData.xdiTemperature);
-
-				break;
-
-			default:
-
-				dataIndex += dataSize;
-				break;
 		}
+
+		i++; //Increment index
 	}
 	return 1;
 }
@@ -260,15 +315,3 @@ void task_mti(void * pvParameters){
 			} //else create a handle for notificationMessage
 		}
 	}
-
-/*
-
-XDI_PacketCounter, 20 , "NPacket", mtiData.xdiPacketCounter, read_packet;
-XDI_Acceleration, 20, "Accel", mtiData.xdiAcceleration, read_acceleration;
-XDI_DeltaV, "dv", 20 , mtiData.xdiDeltaV, read_deltaV;
-XDI_VelocityXYZ, 20, "velocity", mtiData.xdiVelocity, read_velocity;
-XDI_RateOfTurn, 20, "ROT", mtiData.xdiRateOfTurn, read_rateOfTurn;
-XDI_EulerAngles, 20, "angle", mtiData.xdiEulerAngle, read_angle;
-XDI_Temperature, 20, "Temperature", mtiData.xdiTemperature, read_temperature;
-
- */
